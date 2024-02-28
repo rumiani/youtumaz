d4966b7d0044c24576b920a96236e0de7ce87f78
rumiani <maziar.rumiani@gmail.com>
2024-02-29 02:01:48 +0330

    fixed the error type

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 
-export default function Error({error, reset}) {
+export default function Error({error, reset}:{error:any, reset:any}) {
     console.log(error);
     
   return (
