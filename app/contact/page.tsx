723c62b92f57f8d58b243f78a0f9dbefdd7c3b5b
rumiani <maziar.rumiani@gmail.com>
2024-02-29 15:40:07 +0330

    faq and contact us content added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ import contactPicture from "@/public/assets/contact-pic/contact-pic.png";
 export default function Contact() {
   return (
     <div>
-      <div className="w-full h-auto p-8 flex flex-row">
+      <div className="w-full justify-center h-auto p-8 flex flex-row">
         <p className="mr-4 text-lg">You can contact us here:</p>
         <Link
           href="mailto:contact@maziar9170@gmail.com"
@@ -18,7 +18,7 @@ export default function Contact() {
         src={contactPicture}
         width={200}
         alt="Contact us picture"
-        className="text-center mx-auto w-4/5"
+        className="text-center mx-auto w-4/5 md:w-3/5"
       />
     </div>
   );
