5c3e5a2d39decc342032a2eb9fdaf96b272826ca
rumiani <maziar.rumiani@gmail.com>
2024-03-06 15:53:01 +0330

    adding sign out btn

--- app/dashboard-page/page.tsx
+++ app/dashboard-page/page.tsx
@@ -1,10 +1,27 @@
-"use client"
+"use client";
 import React from "react";
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from "next-auth/react";
 import AuthAlert from "../components/authAlert/authAlert";
 
 export default function Dashboard() {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-  return <div>{session ? <div>You are logged in and this is the Dashboard</div> : <AuthAlert/>}</div>;
+  return (
+    <div>
+      {session ? (
+        <div>
+          You are logged in and this is the Dashboard
+          <button
+            onClick={() => signOut()}
+            title="Log in with Google"
+            className="my-16 cursor-pointer text-gray-600 hover:text-gray-800 transition-all duration-300 hover:scale-105 shadow-lg shadow-gray-300 rounded-lg flex flex-row w-60 h-10 mx-auto text-center"
+          >
+            Sign out
+          </button>
+        </div>
+      ) : (
+        <AuthAlert />
+      )}
+    </div>
+  );
 }
