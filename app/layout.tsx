ad31e6f6c8e98d2357d3bd105d87b73d20259171
rumiani <maziar.rumiani@gmail.com>
2024-03-06 15:50:53 +0330

    changing folder names

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 import { NextAuthProvider } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
